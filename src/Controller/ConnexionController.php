e42670076748cfc12842a0254198f7bdb220efff
Alexandre Maitre <alexmaitre38@gmail.com>
2025-01-25 00:10:22 +0100

    test css connexion

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -4,5 +4,8 @@ namespace App\Controller;
 
 class ConnexionController extends BaseController
 {
-
+    public function index() {
+        // Afficher la page d'accueil
+        $this->renderView('connexion.twig');
+    }
 }
