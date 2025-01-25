5f2a5bc108499b380042ea46a6652024e4afe03b
Alexandre Maitre <alexmaitre38@gmail.com>
2025-01-25 12:54:21 +0100

    finalisation connexion

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -2,10 +2,35 @@
 
 namespace App\Controller;
 
+use App\Model\User;
+
 class ConnexionController extends BaseController
 {
-    public function index() {
-        // Afficher la page d'accueil
-        $this->renderView('connexion.twig');
+    private $UserModel;
+
+    public function __construct($pdo){
+        $this->UserModel = new User($pdo);
     }
+
+
+
+    public function index(){
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $login = $_POST['login'];
+            $mdp = $_POST['mdp'];
+
+            if($this->UserModel->checkUser($login, $mdp)){
+                session_start();
+                $_SESSION['isLogged'] = true;
+                header('Location: index.php?page=accueil');
+                exit();
+            } else {
+                $this->renderView('connexion.twig', ['erreur' => 'login, mot de passe ou role incorrect']);
+            }
+        } else {
+            $this->renderView('connexion.twig');
+        }
+    }
+
+
 }
--- src/Model/User.php
+++ src/Model/User.php
@@ -4,4 +4,14 @@ namespace App\Model;
 class User
 {
     private $pdo;
+    public function __construct($pdo){
+        $this->pdo = $pdo;
+    }
+
+    public function checkUser($login, $mdp): bool
+    {
+        $stmt = $this->pdo->prepare("SELECT IdUser FROM user WHERE login = ? AND mdp = ?");
+        $stmt->execute([$login, $mdp]);
+        return !empty($stmt->fetchAll());
+    }
 }
