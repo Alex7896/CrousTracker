ee346f670a3b692bf43ed745130b187c63fac016
Romain <romain.aillaud@free.fr>
2025-01-27 19:34:39 +0100

    commit

--- src/Controller/InfoCrousController.php
+++ src/Controller/InfoCrousController.php
@@ -15,14 +15,14 @@ class InfoCrousController extends BaseController
     }
 
     public function afficherDetails($id) {
-        //$this->renderView('details.twig', ['restaurant' =>$this->restaurantModel->getRestaurantDetails($id)]);
-        var_dump($this->restaurantModel->getRestaurantDetails($id));
+        $this->renderView('details.twig', ['restaurant' =>$this->restaurantModel->getRestaurantDetails($id)]);
     }
 
 
     public function afficherMenu($id) {
         $menu = $this->restaurantModel->getMenu($id);
         $this->renderView('infoCrous/menu.twig', ['menu' => $menu]);
+
     }
 
     public function afficherAvis() {
